36d0ca8baa2807d63ecafeefb560e78f182f6e59
FrederikWA <s144440>
2017-01-09 12:40:27 +0100

    GamePanel push

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -18,10 +18,13 @@ public class MainController {
 	}
 	
 	public void init() {
+		//testgrid
+		int[][] grid = new int[1][1];
+		
 		//Main menu view
 		mMenu = new MainMenu();
 		//Game view
-		gView = new GameView();
+		gView = new GameView(grid);
 		
 		//Action handler for main menu
 		actMainMenu = new ActionHandlerMainMenu(this);
